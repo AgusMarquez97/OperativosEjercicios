10c05afa6790747bc25bbeb0e4e6cd97fc1b5669
= <aguusmarquez@gmail.com>
2018-09-08 12:10:15 -0300

    commit prueba

--- src/Ejercicio0.c
+++ src/Ejercicio0.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+
+
 int main(void) {
-	puts("Hola Mundo"); /* prints Hola Mundo */
-	return EXIT_SUCCESS;
+
+	int * punteroDinamico = malloc(4*sizeof(int));
+	punteroDinamico[0] = 13;
+	punteroDinamico[1] = 15;
+	punteroDinamico[2] = 17;
+	punteroDinamico[3] = 19;
+
+
+	int i = 0;
+	while(i < 4)
+	{
+		printf("%i\n",*(punteroDinamico+i));
+		i++;
+	}
+
+	punteroDinamico = realloc(punteroDinamico,6*sizeof(int));
+
+	punteroDinamico[4] = 12;
+	punteroDinamico[5] = 13;
+	printf("%i\n",*(punteroDinamico+1));
+	printf("%i\n",*(punteroDinamico+4));
+	printf("%i\n",*(punteroDinamico+5));
+
+	free(punteroDinamico);
+
+
 }
